008a848b6a3fe06584fdfed571af88bf5feafeb3
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2017-10-29 20:31:08 +0100

    Podłączenie sterowania kamerą do CameraPreview, liczniki prób połączeń z kamerą, synchronizacja przycisków On/Off i RecognizeMode...
    Dynamiczne tworzenie obiektów + ich kasowanie, porządki

--- LeonCam/CameraPreview.h
+++ LeonCam/CameraPreview.h
@@ -9,15 +9,14 @@
 #include "CapturingFrame.h"
 #include "qsqlquery.h"
 #include "Utilities.h"
-
-#define MAXCONNECTIONTRIES 5
+#include "CameraControl.h"
 
 class CameraPreview : public QDialog
 {
 	Q_OBJECT
 
 public:
-	CameraPreview(QWidget *parent = Q_NULLPTR, QString cameraDetails = "None", QPushButton *buttonIsEnabledFromParent=nullptr, QPushButton *buttonRecognationFromParent = nullptr, QLabel *numberOfEnabledCameras=nullptr, OnvifClientDevice* onvifDevice = nullptr, int camID = -1, std::string passHash = "");
+	CameraPreview(QWidget *parent = Q_NULLPTR, QString cameraDetails = "None", QPushButton *buttonIsEnabledFromParent=nullptr, QPushButton *buttonRecognationFromParent = nullptr, OnvifClientDevice* onvifDevice = nullptr, int camID = -1, std::string passHash = "");
 	~CameraPreview();
 signals:
 	void openCameraEdit(int camID);
@@ -25,9 +24,6 @@ private slots:
 	void BackButtonClicked();
 	void TurnOnOffCamera();
 	void TurnOnOffRecognizeMode();
-	void MoveCamera(float panSpeed, float tiltSpeed);
-	void StopCamera();
-	void GoHomeCamera();
 public slots:
 	void UpdatePixmap(const QPixmap& pixmap);
 	void CloseCameraEdit(const QString& cameraDetails);
@@ -35,13 +31,13 @@ private:
 	Ui::CameraPreview ui;
 	QPushButton *buttonIsEnabledFromParent;
 	QPushButton *buttonRecognationFromParent;
-	QLabel *numberOfEnabledCameras;
 	OnvifClientPTZ *ptz;
 	string profileToken;
 	OnvifClientDevice *onvifDevice;
 	CapturingFrame *capThread;
 	int camID;
 	std::string passHash;
+	CameraControl *ctrl;
 	bool StartShowingPreview();
 	void StopShowingPreview();
 	void closeEvent(QCloseEvent *event);
