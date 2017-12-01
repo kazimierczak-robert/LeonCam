626e5273f5e16b3c9a636aa60381134a5e2f431e
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2017-12-01 16:56:11 +0100

    Zabezpieczenie przed uruchomieniem większej liczby instacji programu

--- LeonCam/main.cpp
+++ LeonCam/main.cpp
@@ -1,12 +1,27 @@
 ﻿#include "LogIn.h"
 #include <QtWidgets/QApplication>
 #include "DataBase.h"
+#include <qlockfile.h>
+//#include <qprocess.h>
 
 std::string Utilities::resultMsg = "";
 
 int main(int argc, char *argv[])
 {
+	/*QProcessEnvironment e = QProcessEnvironment::systemEnvironment();
+	QString ss = e.value("PATH");
+	e.insert("PATH", "$(QTDIR)\bin%3bopencv\x64\vc14\bin%3bopenssl-win64\lib%3b" + ss);
+	QString ss2 = e.value("PATH");*/
+
 	QApplication a(argc, argv);
+
+	QLockFile lockFile(QDir::temp().absoluteFilePath("leoncam.lock"));
+	if (!lockFile.tryLock(100))
+	{
+		Utilities::MBAlarm("The application is already running.", false);
+		return 1;
+	}
+
 	LogIn w;
 	//Create DB file if not exists
 	DataBase *dataBase = new DataBase();
